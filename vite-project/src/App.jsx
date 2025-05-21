dc8d2f8b8114894ad2b15ad37ed13121f27211b6
lleifhei <loganleifheit@gmail.com>
2025-05-20 23:26:12 -0400

    cleanup

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import ReviewDetail from "./routes/ReviewDetail";
+import Reviews from "./routes/Reviews";
+import NewReview from "./routes/NewReview";
 
 function App() {
-  
-
   return (
     <>
-
-    
-
-
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/reviews/:id" element={<ReviewDetail />} />
+          <Route path="/new-review" element={<NewReview />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
