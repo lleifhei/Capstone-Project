aee2780254ea2f256aa79f64d2be6c70a582f895
lleifhei <loganleifheit@gmail.com>
2025-06-11 10:07:35 -0400

    Link was imported from react router twice, removed

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import './Login.css';
-import { Link } from "react-router-dom";
 
 
 const Login = ( { setToken }) => {
